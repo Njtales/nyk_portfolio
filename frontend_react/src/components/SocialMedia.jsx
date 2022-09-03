31cd266801b922f8c155789e17a71bac4205c667
Nikhil Jatale <nikhiljatale@gmail.com>
2022-09-04 00:06:51 +0100

    adding with changes

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BsTwitter, BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs';
+import { FaFacebookF } from 'react-icons/fa';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    {/* <div>
+      <BsTwitter />
+    </div> */}
+    <div>
+      <a href='https://www.facebook.com/nikhiljatale'>
+        <FaFacebookF />
+      </a>
+    </div>
+    <div>
+      <a href='https://www.instagram.com/njtales/'>
+        <BsInstagram />
+      </a>
+    </div>
+    <div>
+      <a href='https://www.linkedin.com/in/nikhil-jatale/'>
+        <BsLinkedin /> 
+      </a>      
+    </div>
+    <div>
+      <a href='https://github.com/Njtales'>
+        <BsGithub />
+      </a>
+    </div>
+  </div>
+);
+
+export default SocialMedia;
---
+++ frontend_react/src/constants/images.js
@@ -0,0 +1,29 @@
+import email from '../assets/email.png';
+import mobile from '../assets/mobile.png';
+
+import git from '../assets/git.png';
+import graphql from '../assets/graphql.png';
+
+import python from '../assets/python.png';
+import dataviz from '../assets/dataviz.png';
+import db from '../assets/db.png';
+// import tempi from '../assets/footercontact_r.png';
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
+import logo from '../assets/logo.png';
+import footerleft from '../assets/footerleft.png';
+
+export default {
+  email,
+  mobile,
+  // tempi,
+  footerleft,
+  git,
+  graphql,
+  dataviz,
+  db,
+  python,
+  profile,
+  circle,
+  logo
+};
