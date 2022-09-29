df45c45efd2cfba4a2032e88001b262523f26446
Nikhil Jatale <nikhiljatale@gmail.com>
2022-09-29 18:32:23 +0100

    updating

--- frontend_react/src/components/index.js
+++ frontend_react/src/components/index.js
@@ -1,11 +1,11 @@
 import NavigationDots from './NavigationDots';
 import SocialMedia from './SocialMedia';
-import Scroll_top from './Scroll_top';
+import Scrolltop from './Scrolltop';
 import Navbar from './Navbar/Navbar';
 
 export {
   NavigationDots,
   SocialMedia,
-  Scroll_top,
+  Scrolltop,
   Navbar,
 };
