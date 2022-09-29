9ca1c3e382fa5513ced264f329ac46700cddfb20
Nikhil Jatale <nikhiljatale@gmail.com>
2022-09-29 18:08:40 +0100

    adding up arrow

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -11,6 +11,7 @@ const NavigationDots = ({ active }) => (
       />
     ))}
   </div>
+
 );
 
 export default NavigationDots;
--- frontend_react/src/components/index.js
+++ frontend_react/src/components/index.js
@@ -1,9 +1,11 @@
 import NavigationDots from './NavigationDots';
 import SocialMedia from './SocialMedia';
+import Scroll_top from './Scroll_top';
 import Navbar from './Navbar/Navbar';
 
 export {
   NavigationDots,
   SocialMedia,
+  Scroll_top,
   Navbar,
 };
